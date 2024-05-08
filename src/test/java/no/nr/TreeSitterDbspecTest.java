5b956e857e716bdf5766f323b8ab553e4dfba5d0
Ivar Rummelhoff <ivar.rummelhoff@nr.no>
2024-05-08 11:03:42 +0200

    Improve test (slightly)

--- src/test/java/no/nr/TreeSitterDbspecTest.java
+++ src/test/java/no/nr/TreeSitterDbspecTest.java
@@ -1,10 +1,31 @@
 package no.nr;
 
 import org.junit.jupiter.api.Test;
+import org.treesitter.TSLanguage;
+import org.treesitter.TSNode;
+import org.treesitter.TSParser;
+import org.treesitter.TSTree;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TreeSitterDbspecTest {
     @Test
     void init() {
-        new TreeSitterDbspec();
+        TSParser parser = new TSParser();
+        TSLanguage dbspec = new TreeSitterDbspec();
+        parser.setLanguage(dbspec);
+        TSTree tree = parser.parseString(null, """
+                Parameters:
+                	host - Hostname of database instance used for staging
+                	port - Port of database instance
+
+                # Name of the staging database
+                Set db = "adventure"
+                """);
+        TSNode rootNode = tree.getRootNode();
+        assertEquals(2, rootNode.getChildCount());
+
+        assertEquals("parameters", rootNode.getChild(0).getType());
+        assertEquals("set", rootNode.getChild(1).getType());
     }
 }

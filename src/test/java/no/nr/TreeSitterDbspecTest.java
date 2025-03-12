fec28c11cbb1a2f3d95fd32e4f54563d167c1916
Ivar Rummelhoff <ivar.rummelhoff@nr.no>
2025-03-12 16:08:59 +0100

    Extend unit test
    There should be more tests...

--- src/test/java/no/nr/TreeSitterDbspecTest.java
+++ src/test/java/no/nr/TreeSitterDbspecTest.java
@@ -1,10 +1,7 @@
 package no.nr;
 
 import org.junit.jupiter.api.Test;
-import org.treesitter.TSLanguage;
-import org.treesitter.TSNode;
-import org.treesitter.TSParser;
-import org.treesitter.TSTree;
+import org.treesitter.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,18 +9,29 @@ class TreeSitterDbspecTest {
     @Test
     void init() {
         TSParser parser = new TSParser();
-        TSLanguage dbspec = new TreeSitterDbspec();
-        parser.setLanguage(dbspec);
-        TSTree tree = parser.parseString(null, "Parameters:\n" +
-                                               "	host - Hostname of database instance used for staging\n" +
-                                               "	port - Port of database instance\n" +
-                                               "\n" +
-                                               "# Name of the staging database\n" +
-                                               "Set db = \"adventure\"\n");
+        parser.setLanguage(new TreeSitterDbspec());
+        String input = "Parameters:\n" +
+                "	host - Hostname of database instance used for staging\n" +
+                "	port - Port of database instance\n" +
+                "\n" +
+                "# æøå\n" +
+                "\r\n" +
+                "# Name of the staging database\n" +
+                "Set db = \"adventure\"\n";
+        TSTree tree = parser.parseString(null, input);
         TSNode rootNode = tree.getRootNode();
         assertEquals(2, rootNode.getChildCount());
 
         assertEquals("parameters", rootNode.getChild(0).getType());
         assertEquals("set", rootNode.getChild(1).getType());
+
+        TSNode x = rootNode.getChild(1).getChildByFieldName("name");
+
+        TSPoint sp = x.getStartPoint();
+        TSPoint ep = x.getEndPoint();
+        assertEquals(7, sp.getRow());
+        assertEquals(7, ep.getRow());
+        String[] inputLines = input.split("\n");
+        assertEquals("db", inputLines[7].substring(sp.getColumn(), ep.getColumn()));
     }
 }
